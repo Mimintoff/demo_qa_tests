835b718b9af408b2bca627d1adcf80d2f2283841
Михаил <mimintoff@mail.ru>
2023-07-28 12:17:12 +0300

    Homework 1: practice-form

---
+++ src/test/java/demoqa/TextBoxTests.java
@@ -0,0 +1,39 @@
+package demoqa;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TextBoxTests {
+
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+    }
+
+    @Test
+    void fillFormTest(){
+        open("https://demoqa.com/text-box");
+        $(".main-header").shouldHave(text("Text Box"));
+
+        $("#userName").setValue("Mikhail Kocherginsky");
+        $("#userEmail").setValue("Koche@mail.ru");
+        $("#currentAddress").setValue("city: Kostroma street:Testovaya  building:1  ");
+        $("#permanentAddress").setValue("city: Kostroma street:Testovaya  building:2  ");
+        $("#submit").click();
+
+        $("#output").shouldBe(visible);
+        $("#output").$("#name").shouldHave((text("Mikhail Kocherginsky")));
+        $("#output #email").shouldHave((text("Koche@mail.ru")));
+
+
+    }
+}
